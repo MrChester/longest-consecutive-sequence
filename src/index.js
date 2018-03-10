c83a1da317c1be5dc165e67ca26c8273086e398b
Eugene Romankov <iromankov2010@gmail.com>
2018-03-11 02:01:23 +0300

    Add longestConsecutiveLength() implementation and mergeSort() implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    const mergeSort = require('./mergeSort');
+    var arrLen = array.length;
+
+    if (arrLen === 0) {
+        return 0;
+    } else if (arrLen === 1) {
+        return 1;
+    }
+
+
+    // for (let i = 0; i < arrLen - 1; i++) {
+    //     if (array[i] <= array[i + 1]) {
+    //         sortArrFlag = true;
+    //     } else {
+    //         sortArrFlag = false;
+    //         break;
+    //     }
+    // }
+
+    var sortedArr = mergeSort(array);
+    // if (!sortArrFlag) {
+    //     sortedArr = mergeSort(array);
+    // } else {
+    //     sortedArr = array;
+    // }
+
+    var consecutiveLengthArr = [],
+        sortedArrLen = sortedArr.length,
+        flag = false,
+        count = 1;
+
+    for (let i = 0; i < sortedArrLen - 1; i++) {
+        if (sortedArr[i + 1] - sortedArr[i] === 1 || sortedArr[i] === sortedArr[i + 1]) {
+            count++;
+            flag = true;
+            if (i === (sortedArrLen - 1)) {
+                count += 1;
+            }
+        } else {
+            flag = false;
+            consecutiveLengthArr.push(count);
+            count = 1;
+        }
+    }
+
+    if (flag) {
+        consecutiveLengthArr.push(count);
+    }
+
+    var maxLen = consecutiveLengthArr[0];
+
+    for (let i = 1; i < consecutiveLengthArr.length; i++) {
+        if (maxLen < consecutiveLengthArr[i]) {
+            maxLen = consecutiveLengthArr[i];
+        }
+    }
+
+    return maxLen;
 }
