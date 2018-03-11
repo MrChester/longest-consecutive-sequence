114fe2c5eaf4aed712943da38e482a5d00f3381e
Eugene Romankov <iromankov2010@gmail.com>
2018-03-11 17:06:23 +0300

    Fix index.js: Change 'if condition' for finding max consecutive sequence.

--- src/index.js
+++ src/index.js
@@ -28,19 +28,19 @@ module.exports = function longestConsecutiveLength(array) {
     }
 
 
-    // Finding msx consecutive sequence.
+    // Finding max consecutive sequence.
     var sortedArrLen = sortedArr.length,
         count = 1,
         maxLen = 0;
 
     for (let i = 0; i < sortedArrLen - 1; i++) {
-        if (sortedArr[i + 1] - sortedArr[i] === 1 || sortedArr[i] === sortedArr[i + 1] || i === sortedArrLen - 1) {
+        if (sortedArr[i + 1] - sortedArr[i] === 1) {
             count++;
         } else {
-            if (maxLen < count) {
-                maxLen = count;
-            }
             count = 1;
+        }
+        if (maxLen < count) {
+            maxLen = count;
         }
     }
 
